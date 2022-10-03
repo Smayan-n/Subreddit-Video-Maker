80c46e7b685332134f71f3ae808568eaa35bbbed
Smayan Nirantare <86111841+Smayan-n@users.noreply.github.com>
2022-10-03 16:40:10 -0700

    Update constants.py

--- constants.py
+++ constants.py
@@ -7,5 +7,5 @@ COMMENT_SEARCH_LIMIT = 50
 #videoMaker.py
 TEXT_FONT_SIZE = 50
 MIN_VIDEO_DURATION = 50 #seconds
-BOX_RELIEF =  60 #relif for black text box
+BOX_RELIEF =  60 #relief for black text box
 MAX_CHARS = 500 #max amount of chars allowed in a text (comment)
